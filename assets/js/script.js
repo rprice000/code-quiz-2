c66b07e0fd6186082033a754f8089e2ccb10bbb2
rprice000 <rtprice21@gmail.com>
2022-03-04 10:37:48 -0600

    created first DOM elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
 // js for the app
+var appContainer = document.querySelector('#appContainer');
+var openingContent = document.querySelector('#openingContent');
+var quizContent = document.querySelector('#quizContent');
+var startQuizbtn = document.querySelector('#startQuizButton');
+
+
+
+
+startQuizbtn.addEventListener("click", function() {
+    alert("button clicked");
+    openingContent.remove()
+    var question = document.createElement('h1');
+    question.textContent = "hello";
+    quizContent.appendChild(question);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// When user clicks Start Quiz
+// Then main content is removed
+// The timer starts counting down
+// The first question is displayed
+//
